15289225411aceeefb372879724031f4153fc326
Maciej Rożnawski <intro202@wp.pl>
2021-09-26 19:30:04 +0200

    test1

---
+++ myapp/src/components/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        witaj
+      </div>
+    )
+  }
+}
+
+export default App;
